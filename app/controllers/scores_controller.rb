a282818d40efa6e14af8262a6f6b561f84e73d76
Jesse Spevack <jspevack@gmail.com>
2023-11-27 10:22:07 -0700

    Fix

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,6 +8,6 @@ class ScoresController < ApplicationController
 
   def show
     Rails.logger.info("current_player = #{@current_player.inspect}")
-    @aggregate = PlayerProblemAggregate.find_by(player: @current_player, problem: Problem.find_by(id: params[:id]))
+    @aggregate = PlayerProblemAggregate.find_by(player: @current_player, problem: Problem.find_by(id: params[:id])) || PlayerProblemAggregate.new(player: @current_player, problem: Problem.find_by(id: params[:id]), attempts: 0, correct: 0, min_time: 0, max_time: 0, average_time: 0)
   end
 end
--- app/models/player_problem_aggregate.rb
+++ app/models/player_problem_aggregate.rb
@@ -8,16 +8,20 @@ class PlayerProblemAggregate < ApplicationRecord
 
   sig { returns(Integer) }
   def percent_correct
+    return 0 if attempts.zero?
+
     ((correct.to_f / attempts) * 100).to_i
   end
 
   sig { returns(T::Boolean) }
   def proficient?
-    ((correct.to_f / attempts) * 100).to_i >= 85
+    percent_correct >= 85
   end
 
   sig { returns(T::Boolean) }
   def fast?
+    return false if attempts.zero?
+
     average_time <= 5
   end
 end
