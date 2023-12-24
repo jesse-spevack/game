a8897863cd1b9ee81789326239381889a4e36216
Jesse Spevack <jspevack@gmail.com>
2023-12-24 09:15:14 -0700

    Change drive

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  DRIVER = if ENV["DRIVER"]
+    ENV["DRIVER"].to_sym
+  else
+    :headless_chrome
+  end
+
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
 end
