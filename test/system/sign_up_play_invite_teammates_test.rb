53cc0886e19935f6dd5866820a4217fd161c5bea
Jesse Spevack <jspevack@gmail.com>
2023-12-29 22:02:25 -0700

    Update system test

--- test/system/sign_up_play_invite_teammates_test.rb
+++ test/system/sign_up_play_invite_teammates_test.rb
@@ -60,55 +60,7 @@ class SignUpPlayInviteTeammatesTest < ApplicationSystemTestCase
 
     assert_text("#{name}!'s progress")
 
-    # Play a game
-    click_link "1 + 1"
-    within("h3") do
-      click_link "1 + 1"
-    end
-    click_button "2"
-    click_button "☑"
-
-    # Check score
-    click_on "#{name}!'s scores"
-    assert_text("1\nproblems solved")
-    assert_text("1\nday in a row")
-
-    click_on "Play"
-
-    x_element = find(:xpath, "/html/body/main/div/div/div[1]/div[2]/p")
-    x_text = x_element.text
-    x = x_text.to_i
-
-    y_element = find(:xpath, "/html/body/main/div/div/div[1]/div[3]/p")
-    y_text = y_element.text
-    y = y_text.to_i
-
-    this_problem = Problem.find_by(x: x, y: y)
-
-    take_screenshot
-    click_button((x + y).to_s)
-    click_button "☑"
-
-    click_on "#{name}!'s scores"
-    take_screenshot
-    click_button "Play"
-
-    take_screenshot
-    x_element = find(:xpath, "/html/body/main/div/div/div[1]/div[2]/p")
-    x_text = x_element.text
-    new_x = x_text.to_i
-
-    y_element = find(:xpath, "/html/body/main/div/div/div[1]/div[3]/p")
-    y_text = y_element.text
-    new_y = y_text.to_i
-
-    this_new_problem = Problem.find_by(x: new_x, y: new_y)
-
-    assert_not_equal(this_problem, this_new_problem)
-
-    click_on "#{name}!'s scores"
-    assert_text("2\nproblems solved")
-    assert_text("1\nday in a row")
+    assert_game_plays_as_expected(player: player)
 
     assert_equal(0, Invite.count)
 
@@ -164,5 +116,74 @@ class SignUpPlayInviteTeammatesTest < ApplicationSystemTestCase
     assert_text("Players")
 
     take_screenshot
+  end
+
+  def assert_game_plays_as_expected(player:)
+    click_button "Play"
+
+    3.times do
+      x_element = find(:xpath, "/html/body/main/div/div/div[1]/div[2]/p")
+      x_text = x_element.text
+      x = x_text.to_i
+
+      y_element = find(:xpath, "/html/body/main/div/div/div[1]/div[3]/p")
+      y_text = y_element.text
+      y = y_text.to_i
+
+      this_problem = Problem.find_by(x: x, y: y)
+      click_button((x + y).to_s)
+
+      # Find submit button and click it.
+      find(:xpath, "/html/body/main/div/div/form/div/input").click
+      take_screenshot
+
+      last_response = Response.where(player: player).order(created_at: :desc).first
+      assert_equal(this_problem, last_response.problem)
+      assert_equal(this_problem.solution, last_response.value)
+      assert(last_response.correct)
+      assert(last_response.started_at)
+      assert(last_response.completed_at)
+
+      player_problem_aggregate = PlayerProblemAggregate.find_by(player: player, problem: this_problem)
+      assert_equal(1, player_problem_aggregate.attempts)
+      assert_equal(1, player_problem_aggregate.correct)
+
+      x_element = find(:xpath, "/html/body/main/div/div/div[1]/div[2]/p")
+      x_text = x_element.text
+      new_x = x_text.to_i
+
+      y_element = find(:xpath, "/html/body/main/div/div/div[1]/div[3]/p")
+      y_text = y_element.text
+      new_y = y_text.to_i
+
+      this_new_problem = Problem.find_by(x: new_x, y: new_y)
+
+      # Problems should be different
+      assert_not_equal(this_problem, this_new_problem)
+
+      click_button((new_x + new_y).to_s)
+
+      find(:xpath, "/html/body/main/div/div/form/div/input").click
+      take_screenshot
+
+      player.reload
+      last_response = player.responses.last
+      assert_equal(this_new_problem, last_response.problem)
+      assert_equal(this_new_problem.solution, last_response.value)
+      assert(last_response.correct)
+      assert(last_response.started_at)
+      assert(last_response.completed_at)
+
+      player_problem_aggregate = PlayerProblemAggregate.find_by(player: player, problem: this_problem)
+      assert_equal(1, player_problem_aggregate.attempts)
+      assert_equal(1, player_problem_aggregate.correct)
+    end
+
+    # Check score
+    take_screenshot
+    # find(:xpath, "/html/body/main/nav/div/div/div/div/a[6]").click
+    click_link "#{player.name}'s scores"
+    assert_text("6\nproblems solved")
+    assert_text("1\nday in a row")
   end
 end
