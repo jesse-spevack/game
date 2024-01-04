2593441f3eb1766b22d72a582e22ea2bf2a0c53b
Jesse Spevack <jspevack@gmail.com>
2024-01-03 18:17:12 -0700

    FE validation on response max

--- app/javascript/controllers/hidden_input_controller.js
+++ app/javascript/controllers/hidden_input_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="hidden-input"
 export default class extends Controller {
-  static targets = ["display", "field", "submit"]
+  static targets = ["bigError", "display", "field", "submit"]
   static values = {
     response: { type: String, default: "" },
   }
@@ -32,8 +32,13 @@ export default class extends Controller {
 
     if (this.responseValue.length === 0) {
       this.submitTarget.disabled = true;
+      this.bigErrorTarget.classList.add("hidden")
+    } else if (this.responseValue.length === 4) {
+      this.bigErrorTarget.classList.remove("hidden")
+      this.submitTarget.disabled = true;
     } else {
       this.submitTarget.disabled = false;
+      this.bigErrorTarget.classList.add("hidden")
     }
 
   }
@@ -46,8 +51,10 @@ export default class extends Controller {
   }
 
   appendInput(currentResponse, userInput) {
-    this.responseValue = currentResponse + userInput 
-    this.displayTarget.innerText = this.responseValue 
-    this.fieldTarget.value = this.responseValue 
+    if (currentResponse.length < 4) {
+      this.responseValue = currentResponse + userInput 
+      this.displayTarget.innerText = this.responseValue 
+      this.fieldTarget.value = this.responseValue 
+    }
   }
 }
--- test/system/sign_up_play_invite_teammates_test.rb
+++ test/system/sign_up_play_invite_teammates_test.rb
@@ -183,7 +183,7 @@ class SignUpPlayInviteTeammatesTest < ApplicationSystemTestCase
     click_button(0.to_s)
     click_button(1.to_s)
     find(:xpath, "/html/body/main/div/div/form/div/input").click
-    assert_text("Try again")
+    assert_text("can't be that big.")
 
     # Check score
     take_screenshot
