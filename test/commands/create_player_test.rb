432ee420b1fe4c8b192f0ff167dd27e89f93a639
Jesse Spevack <jspevack@gmail.com>
2024-01-28 17:38:21 -0700

    Fix test

--- test/commands/create_player_test.rb
+++ test/commands/create_player_test.rb
@@ -3,21 +3,14 @@
 require "test_helper"
 
 class Commands::CreatePlayerTest < ActiveSupport::TestCase
-  include ActiveJob::TestHelper
-
   test "it creates a player" do
     input = ActionController::Parameters.new(name: "name", team_id: teams(:one).id)
-    player = perform_enqueued_jobs do
-      result = Commands::CreatePlayer.call(input: input)
+    result = Commands::CreatePlayer.call(input: input)
 
-      assert_kind_of(Player, result)
-      assert(1, result.level)
-      assert("name", result.name)
-      assert(teams(:one), result.team)
-      result
-    end
-
-    assert_performed_with(job: CreatePlayerProblemAggregatesJob, args: [player_id: player.id])
-    assert(PlayerProblemAggregate.where(player: player).count > 0)
+    assert_kind_of(Player, result)
+    assert(1, result.level)
+    assert("name", result.name)
+    assert(teams(:one), result.team)
+    assert(PlayerProblemAggregate.where(player: result).count > 0)
   end
 end
