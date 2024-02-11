b75bd5aa8813de13f77f264fb603281d8ee98f17
Jesse Spevack <jspevack@gmail.com>
2024-02-11 09:22:09 -0700

    More components

--- app/components/table_data_bold_component.rb
+++ app/components/table_data_bold_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TableDataBoldComponent < ViewComponent::Base
-  def initialize(data:)
+  def initialize(data:, collapsable: false)
     @data = data
   end
 end
---
+++ test/components/tiny_table_datum_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TinyTableDatumComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(TinyTableDatumComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
