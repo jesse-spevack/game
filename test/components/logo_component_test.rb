c089a2c449743065bedd5a086897d61f316396da
Jesse Spevack <jspevack@gmail.com>
2024-04-06 12:00:20 -0600

    Update logo

---
+++ test/components/logo_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class LogoComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(LogoComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
