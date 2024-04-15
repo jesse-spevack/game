e5f506e2d4f238592118c02f10b74352147d838d
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-04-15 14:10:47 -0500

    test: create toggle component test (#22)

--- test/components/toggle_component_test.rb
+++ test/components/toggle_component_test.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
 class ToggleComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(ToggleComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def setup
+    @form = mock()
+    @form.expects(:hidden_field).with("Field", value: "Default Value", data: { toggle_target: "hiddenField" })
+  end
+
+  def test_component_renders_something_useful_with_description
+    toggle_component = render_inline(ToggleComponent.new(label: "Label", form: @form, field: "Field", default_value: "Default Value", description: "Description"))
+
+    assert_selector("#availability-label", text: "Label")
+    assert_selector("#availability-description", text: "Description")
+  end
+
+  def test_component_renders_something_useful_without_description
+    toggle_component = render_inline(ToggleComponent.new(label: "Label", form: @form, field: "Field", default_value: "Default Value"))
+
+    assert_selector("#availability-label", text: "Label")
+    refute_selector("#availability-description")
   end
 end
